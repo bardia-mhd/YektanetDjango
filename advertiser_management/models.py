c0b3c074d687ed22c90492ed593631998677a2f2
bardia-mhd <Bbardia2001>
2021-02-03 17:05:22 +0330

    Add click and view classes

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -2,43 +2,70 @@ from django.db import models
 
 # Create your models here.
 from django.db.models import Sum
+from django.utils.datetime_safe import datetime
 
 
 class Advertiser(models.Model):
-    clicks = models.PositiveIntegerField(default=0)
-    views = models.PositiveIntegerField(default=0)
     name = models.CharField(max_length=150)
 
     @staticmethod
     def get_total_clicks():
-        return Advertiser.objects.aggregate(Sum('clicks'))
-
-    def inc_clicks(self):
-        self.clicks += 1
-        self.save()
+        return Advertiser.objects.aggregate(Sum('clicks')).get('clicks__sum')
 
     def inc_views(self):
-        self.views += 1
+        sum = 0
+        for ad in self.ad_set.all():
+            sum += ad.get_clicks()
+        return sum
+
+    def get_views(self):
+        sum = 0
+        for ad in self.ad_set.all():
+            sum += ad.get_views()
+        return sum
 
 
 class Ad(models.Model):
     title = models.CharField(max_length=150)
     imgUrl = models.CharField(max_length=300)
     img = models.ImageField(upload_to='images', default='default.jpg')
-    link = models.CharField(max_length=300)
-    clicks = models.PositiveIntegerField(default=0)
-    views = models.PositiveIntegerField(default=0)
+    link = models.URLField(max_length=300)
     advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+    approve = models.BooleanField(default=False)
 
-    def inc_clicks(self):
-        self.advertiser.inc_clicks()
-        self.clicks += 1
-        self.save()
-        self.advertiser.save()
+    def get_clicks(self):
+        return self.click_set.count()
 
-    def inc_views(self):
-        self.advertiser.inc_views()
-        self.views += 1
-        self.save()
-        self.advertiser.save()
+    def inc_clicks(self, ip):
+        obj1 = Click.objects.create(
+            ad=self,
+            time=datetime.now(),
+            user_ip=ip
+        )
+        obj1.save()
 
+    def inc_views(self, ip):
+        obj1 = Click.objects.create(
+            ad=self,
+            time=datetime.now(),
+            user_ip=ip
+        )
+        obj1.save()
+
+    def get_views(self):
+        return self.view_set.count()
+
+    def is_approve(self):
+        return self.approve
+
+
+class View(models.Model):
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    time = models.DateTimeField(default=datetime.now)
+    user_ip = models.GenericIPAddressField()
+
+
+class Click(models.Model):
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    time = models.DateTimeField(default=datetime.now)
+    user_ip = models.GenericIPAddressField()

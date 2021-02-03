d55387a43c8737fd22cb5d04519ddf6f7cfe8b3c
bardia-mhd <Bbardia2001>
2021-02-03 18:52:07 +0330

    Add features to admin panel

--- advertiser_management/admin.py
+++ advertiser_management/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Ad, Advertiser
 
-admin.site.register(Ad)
-admin.site.register(Advertiser)
+
+class AdminAdModel(admin.ModelAdmin):
+    list_display = ('title', 'approve')
+    list_filter = ('approve', 'advertiser')
+    search_fields = ('title',)
+
+
+class AdminAdvertiserModel(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Ad, AdminAdModel)
+admin.site.register(Advertiser, AdminAdvertiserModel)
